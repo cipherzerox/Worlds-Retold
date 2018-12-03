af8850ec7deda7b372fc62ba81ed410672a073d6
TheMinecraftOverlordYT <kyurembeats2@gmail.com>
2018-12-03 18:35:34 -0500

    Fixed roller attack reach

--- src/main/java/xenoform/hailstorm/MVillagerTrades.java
+++ src/main/java/xenoform/hailstorm/MVillagerTrades.java
@@ -1,7 +1,5 @@
 package xenoform.hailstorm;
 
-import java.util.Random;
-
 import net.minecraft.entity.IMerchant;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Items;
@@ -12,6 +10,8 @@ import net.minecraft.village.MerchantRecipeList;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.VillagerRegistry;
 
+import java.util.Random;
+
 public class MVillagerTrades implements EntityVillager.ITradeList {
 	
 	public static final VillagerRegistry.VillagerProfession priest = ForgeRegistries.VILLAGER_PROFESSIONS
@@ -21,7 +21,7 @@ public class MVillagerTrades implements EntityVillager.ITradeList {
 	public void addMerchantRecipe(IMerchant merchant, MerchantRecipeList recipeList, Random random) {
 		recipeList.add(new MerchantRecipe(new ItemStack(Items.EMERALD, random.nextInt(5) + 4), new ItemStack(MItems.CRYONITE, 1)));
 	}
-	
+
 	public static void registerTrades() {
 		priest.getCareer(1).addTrade(3, new MVillagerTrades());
 	}
--- src/main/java/xenoform/hailstorm/entity/EntitySnowRoller.java
+++ src/main/java/xenoform/hailstorm/entity/EntitySnowRoller.java
@@ -3,31 +3,29 @@ package xenoform.hailstorm.entity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.*;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import xenoform.hailstorm.Hailstorm;
 
-public class EntitySnowRoller extends EntityMob {
+public class EntitySnowRoller extends EntityMob
+{
 	private float size = 1;
 
-	private static final DataParameter<Float> SIZE = EntityDataManager.<Float>createKey(EntitySnowRoller.class,
-			DataSerializers.FLOAT);
-	private static final DataParameter<Boolean> SHRINK = EntityDataManager.<Boolean>createKey(EntitySnowRoller.class,
-			DataSerializers.BOOLEAN);
+	private static final DataParameter<Float> SIZE = EntityDataManager.<Float>createKey(EntitySnowRoller.class, DataSerializers.FLOAT);
+	private static final DataParameter<Boolean> SHRINK = EntityDataManager.<Boolean>createKey(EntitySnowRoller.class, DataSerializers.BOOLEAN);
 
 	public EntitySnowRoller(World world) {
 		super(world);
@@ -36,8 +34,8 @@ public class EntitySnowRoller extends EntityMob {
 	protected void initEntityAI() {
 		this.tasks.addTask(1, new EntityAIWanderAvoidWater(this, 1.0D));
 		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
-		this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 16.0F));
-		this.tasks.addTask(2, new EntityAIAttackMelee(this, 0.8D, false));
+		this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 32.0F));
+		this.tasks.addTask(2, new EntityAIRollerAttack(this, 0.8D, false));
 	}
 
 	protected void applyEntityAttributes() {
@@ -69,13 +67,13 @@ public class EntitySnowRoller extends EntityMob {
 	}
 
 	@Override
-	public void onLivingUpdate() {
+	public void onLivingUpdate()
+    {
 		super.onLivingUpdate();
 
-		this.setSize(getSize() * 0.5F, getSize() * 0.5F);
-		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(4.0D + getSize());
-		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED)
-				.setBaseValue(0.23000000417232513D + getSize() / 8);
+        this.setSize(getSize(), getSize());
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(4.0D + getSize());
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23000000417232513D + getSize() / 8);
 
 		if (!world.isRemote) {
 			int i, j, k;
@@ -94,7 +92,27 @@ public class EntitySnowRoller extends EntityMob {
 				}
 			}
 		}
-	}
+    }
+
+/*
+    @Override
+    public void onCollideWithPlayer(EntityPlayer entityIn)
+    {
+        super.onCollideWithPlayer(entityIn);
+
+        if(this.world.isRemote)
+        {
+            this.world.playSound(entityIn, this.getPosition(), SoundEvents.BLOCK_SNOW_BREAK, SoundCategory.HOSTILE, 10F, 1F);
+
+            for (int i = 0; i < 100; ++i)
+            {
+                this.world.spawnParticle(EnumParticleTypes.SNOW_SHOVEL, this.posX + (this.rand.nextDouble() - 0.5D) * (double)this.width,
+                        this.posY + this.rand.nextDouble() * (double)this.height, this.posZ + (this.rand.nextDouble() - 0.5D) * (double)this.width,
+                        (this.rand.nextDouble() - 0.5D), -this.rand.nextDouble(), (this.rand.nextDouble() - 0.5D));
+            }
+        }
+    }
+*/
 
 	public boolean attackEntityAsMob(Entity entityIn) {
 		boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this),
@@ -109,17 +127,7 @@ public class EntitySnowRoller extends EntityMob {
 				this.motionZ *= 0.6D;
 			}
 
-			entityIn.playSound(SoundEvents.BLOCK_SNOW_BREAK, 10F, 1F);
-
-			for (int i = 0; i < 100; ++i) {
-				this.world.spawnParticle(EnumParticleTypes.SNOW_SHOVEL,
-						this.posX + (this.rand.nextDouble() - 0.5D) * (double) this.width,
-						this.posY + this.rand.nextDouble() * (double) this.height,
-						this.posZ + (this.rand.nextDouble() - 0.5D) * (double) this.width,
-						(this.rand.nextDouble() - 0.5D), -this.rand.nextDouble(), (this.rand.nextDouble() - 0.5D));
-			}
-
-			entityIn.attackEntityFrom(Hailstorm.ROLLER, 1 + getSize() * 2);
+            entityIn.attackEntityFrom(Hailstorm.ROLLER, 1 + getSize() * 2);
 
 			if (getSize() > 1)
 				this.dropItem(Items.SNOWBALL, (int) getSize() * 2);
@@ -128,8 +136,17 @@ public class EntitySnowRoller extends EntityMob {
 			size = 1;
 		}
 		return flag;
-
 	}
+
+    @Override
+    protected void damageEntity(DamageSource damageSrc, float damageAmount) {
+        if(damageSrc == DamageSource.ON_FIRE || damageSrc == DamageSource.ON_FIRE || damageSrc == DamageSource.HOT_FLOOR)
+            super.damageEntity(damageSrc, damageAmount * 2);
+        else if(damageSrc == DamageSource.LAVA)
+            super.damageEntity(damageSrc, damageAmount * 3);
+        else
+            super.damageEntity(damageSrc, damageAmount);
+    }
 
 	@Override
 	public void writeEntityToNBT(NBTTagCompound compound) {
