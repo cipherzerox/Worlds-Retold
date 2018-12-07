9e842a213951bf0a9570cb6695b8967509cd3969
gegy1000 <gegy1000@gmail.com>
2018-12-07 19:02:48 +0200

    Fix tabs

--- src/main/java/xenoform/hailstorm/world/StructureHailstormShrine.java
+++ src/main/java/xenoform/hailstorm/world/StructureHailstormShrine.java
@@ -23,57 +23,57 @@ import java.util.Random;
 
 public class StructureHailstormShrine extends WorldGenerator {
 
-    @Override
-    public boolean generate(World world, Random rand, BlockPos origin) {
-        WorldServer worldServer = (WorldServer) world;
-        MinecraftServer server = world.getMinecraftServer();
-        TemplateManager templateManager = worldServer.getStructureTemplateManager();
-        Template template = templateManager.getTemplate(server,
-                new ResourceLocation(Hailstorm.MODID + ":hailstorm_shrine"));
+	@Override
+	public boolean generate(World world, Random rand, BlockPos origin) {
+		WorldServer worldServer = (WorldServer) world;
+		MinecraftServer server = world.getMinecraftServer();
+		TemplateManager templateManager = worldServer.getStructureTemplateManager();
+		Template template = templateManager.getTemplate(server,
+				new ResourceLocation(Hailstorm.MODID + ":hailstorm_shrine"));
 
-        if (template == null) {
-            System.out.println("NO STRUCTURE");
-            return false;
-        }
+		if (template == null) {
+			System.out.println("NO STRUCTURE");
+			return false;
+		}
 
-        PlacementSettings settings = (new PlacementSettings())
-                .setMirror(Mirror.values()[rand.nextInt(Mirror.values().length)])
-                .setRotation(Rotation.values()[rand.nextInt(Rotation.values().length)])
-                .setIgnoreEntities(false);
+		PlacementSettings settings = (new PlacementSettings())
+				.setMirror(Mirror.values()[rand.nextInt(Mirror.values().length)])
+				.setRotation(Rotation.values()[rand.nextInt(Rotation.values().length)])
+				.setIgnoreEntities(false);
 
-        BlockPos size = template.getSize();
-        BlockPos generatePos = center(settings, origin, size.getX(), size.getZ());
+		BlockPos size = template.getSize();
+		BlockPos generatePos = center(settings, origin, size.getX(), size.getZ());
 
-        BlockPos max = generatePos.add(Template.transformedBlockPos(settings, template.getSize()));
-        if (WorldGenHailstorm.canSpawnHere(worldServer, generatePos, max)
-            // && world.getBiome(position) instanceof Biomes.
-        ) {
-            System.out.println("X:" + origin.getX() + " Y:" + origin.getY() + " Z:" + origin.getZ());
+		BlockPos max = generatePos.add(Template.transformedBlockPos(settings, template.getSize()));
+		if (WorldGenHailstorm.canSpawnHere(worldServer, generatePos, max)
+			// && world.getBiome(position) instanceof Biomes.
+		) {
+			System.out.println("X:" + origin.getX() + " Y:" + origin.getY() + " Z:" + origin.getZ());
 
-            template.addBlocksToWorld(world, generatePos.up(), settings);
+			template.addBlocksToWorld(world, generatePos.up(), settings);
 
-            Map<BlockPos, String> dataBlocks = template.getDataBlocks(generatePos, settings);
-            for (Entry<BlockPos, String> entry : dataBlocks.entrySet()) {
-                handleDataBlock(world, rand, entry.getKey(), entry.getValue());
-            }
+			Map<BlockPos, String> dataBlocks = template.getDataBlocks(generatePos, settings);
+			for (Entry<BlockPos, String> entry : dataBlocks.entrySet()) {
+				handleDataBlock(world, rand, entry.getKey(), entry.getValue());
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    private void handleDataBlock(World world, Random rand, BlockPos pos, String key) {
-        if ("chest".equals(key)) {
-            world.setBlockState(pos.up(), Blocks.AIR.getDefaultState(), 3);
-            TileEntity entity = world.getTileEntity(pos);
-            if (entity instanceof TileEntityChest) {
-                ((TileEntityChest) entity).setLootTable(LootTableList.CHESTS_SIMPLE_DUNGEON, rand.nextLong());
-            }
-        }
-    }
+	private void handleDataBlock(World world, Random rand, BlockPos pos, String key) {
+		if ("chest".equals(key)) {
+			world.setBlockState(pos.up(), Blocks.AIR.getDefaultState(), 3);
+			TileEntity entity = world.getTileEntity(pos);
+			if (entity instanceof TileEntityChest) {
+				((TileEntityChest) entity).setLootTable(LootTableList.CHESTS_SIMPLE_DUNGEON, rand.nextLong());
+			}
+		}
+	}
 
-    private BlockPos center(PlacementSettings settings, BlockPos pos, int sizeX, int sizeZ) {
-        return pos.subtract(Template.transformedBlockPos(settings, new BlockPos(sizeX / 2, 0, sizeZ / 2)));
-    }
+	private BlockPos center(PlacementSettings settings, BlockPos pos, int sizeX, int sizeZ) {
+		return pos.subtract(Template.transformedBlockPos(settings, new BlockPos(sizeX / 2, 0, sizeZ / 2)));
+	}
 }
