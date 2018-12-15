294b5fc33cb4cdefc6bcb8ddf42f08b56e7e5bb1
Xenoform55 <arrowstorm606@gmail.com>
2018-12-15 02:26:31 -0500

    Added Durability To Blizz Scroll

--- src/main/java/xenoform/hailstorm/base/BasicItemBlizzScroll.java
+++ src/main/java/xenoform/hailstorm/base/BasicItemBlizzScroll.java
@@ -3,6 +3,7 @@ package xenoform.hailstorm.base;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -18,6 +19,7 @@ public class BasicItemBlizzScroll extends BasicItem {
 		super(name);
 		this.name = name;
 		this.maxStackSize = 1;
+        this.setMaxDamage(3);
 		this.setCreativeTab(CreativeTabs.COMBAT);
 	}
 
@@ -42,6 +44,7 @@ public class BasicItemBlizzScroll extends BasicItem {
 
 	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving) {
 		float speed = 2F;
+		entityLiving.playSound(SoundEvents.ENTITY_WITHER_AMBIENT, 1.0F, 0.5F);
 		for (int i = 0; i < 360; i += 7.5) {
 			EntityIceScrollProjectile ent = new EntityIceScrollProjectile(worldIn);
 			ent.motionX = Math.sin((float) Math.toRadians(i)) * speed / 8;
@@ -50,6 +53,7 @@ public class BasicItemBlizzScroll extends BasicItem {
 			ent.setPosition(entityLiving.posX + Math.sin(Math.toRadians(i)), entityLiving.posY + 1, entityLiving.posZ + Math.cos(Math.toRadians(i)));
 			worldIn.spawnEntity(ent);
 		}
+		stack.damageItem(1, entityLiving);
 		return stack;
 	}
 }
