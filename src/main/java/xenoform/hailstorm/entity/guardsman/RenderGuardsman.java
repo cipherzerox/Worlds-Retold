cab7a9296be384d28dcce341f2dc16d7fd7e0a8a
Xenoform55 <arrowstorm606@gmail.com>
2018-12-15 20:41:06 -0500

    Active Texture

--- src/main/java/xenoform/hailstorm/entity/guardsman/RenderGuardsman.java
+++ src/main/java/xenoform/hailstorm/entity/guardsman/RenderGuardsman.java
@@ -6,24 +6,34 @@ import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
+import xenoform.hailstorm.entity.layer.LayerAutomatonGlow;
+import xenoform.hailstorm.entity.layer.LayerGuardsmanGlow;
 
 import javax.annotation.Nonnull;
 
 public class RenderGuardsman extends RenderLiving<EntityGuardsman>
 {
     private ResourceLocation TEXTURE = new ResourceLocation("hailstorm:textures/entity/guardsman.png");
+    private ResourceLocation SHOOTING_TEXTURE = new ResourceLocation("hailstorm:textures/entity/guardsman_charging.png");
+	private ResourceLocation GLOW_TEXTURE = new ResourceLocation("hailstorm:textures/entity/guardsman_glow.png");
+    
     public static final RenderGuardsman.Factory FACTORY = new RenderGuardsman.Factory();
 
     public RenderGuardsman(RenderManager renderManagerIn)
     {
         super(renderManagerIn, new ModelGuardsman(), 0.55F);
+		this.addLayer(new LayerGuardsmanGlow(this, GLOW_TEXTURE));
     }
 
     @Override
     @Nonnull
     public ResourceLocation getEntityTexture(@Nonnull EntityGuardsman entity)
     {
-        return TEXTURE;
+		if (entity.getCharging()) {
+			return SHOOTING_TEXTURE;
+		} else {
+			return TEXTURE;
+		}
     }
 
     @Override
