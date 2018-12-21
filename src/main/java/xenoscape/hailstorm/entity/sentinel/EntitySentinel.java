aa26e458047311a989fb0909c6e03dc81c098ed0
TheMinecraftOverlordYT <kyurembeats2@gmail.com>
2018-12-20 22:24:29 -0500

    Working on Sentinel poses

--- src/main/java/xenoscape/hailstorm/entity/sentinel/EntitySentinel.java
+++ src/main/java/xenoscape/hailstorm/entity/sentinel/EntitySentinel.java
@@ -1,25 +1,36 @@
 package xenoscape.hailstorm.entity.sentinel;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import xenoscape.hailstorm.entity.automaton.EntityAutomaton;
 import xenoscape.hailstorm.entity.guardsman.EntityGuardsman;
 
 import java.util.List;
 
 public class EntitySentinel extends EntityGuardsman
 {
-    protected static final DataParameter<Boolean> ACTIVE = EntityDataManager.createKey(EntityAutomaton.class, DataSerializers.BOOLEAN);
-    protected float targetDistance;
+    private static final DataParameter<Boolean> ACTIVE = EntityDataManager.createKey(EntitySentinel.class, DataSerializers.BOOLEAN);
+    private static final DataParameter<Boolean> SHOULD_EXPLODE = EntityDataManager.createKey(EntitySentinel.class, DataSerializers.BOOLEAN);
+    private static final DataParameter<Boolean> HAS_EXPLODED = EntityDataManager.createKey(EntitySentinel.class, DataSerializers.BOOLEAN);
+    private float targetDistance;
 
     public EntitySentinel(World world){
         super(world);
         setSize(2, 2);
+    }
+
+    @Override
+    protected void entityInit() {
+        super.entityInit();
+        dataManager.register(ACTIVE, false);
+        dataManager.register(SHOULD_EXPLODE, false);
+        dataManager.register(HAS_EXPLODED, false);
     }
 
     public boolean isActive() {
@@ -30,9 +41,25 @@ public class EntitySentinel extends EntityGuardsman
         getDataManager().set(ACTIVE, isActive);
     }
 
+    public boolean shouldExplode() {
+        return getDataManager().get(SHOULD_EXPLODE);
+    }
+
+    public void setShouldExplode(boolean shouldExplode) {
+        getDataManager().set(SHOULD_EXPLODE, shouldExplode);
+    }
+
+    public boolean hasExploded() {
+        return getDataManager().get(HAS_EXPLODED);
+    }
+
+    public void setHasExploded(boolean hasExploded) {
+        getDataManager().set(HAS_EXPLODED, hasExploded);
+    }
+
     @Override
     public void onLivingUpdate() {
-        setActive(false);
+      //  setActive(false);
         super.onLivingUpdate();
         if (getAttackTarget() == null) {
             List<EntityPlayer> list = this.world.<EntityPlayer>getEntitiesWithinAABB(EntityPlayer.class,
@@ -54,8 +81,22 @@ public class EntitySentinel extends EntityGuardsman
                         setActive(false);
                     }
                 } else if (getAttackTarget() != null && targetDistance <= 5) {
+                    if(!hasExploded()){
+                        setShouldExplode(true);
+                        setHasExploded(true);
+                    }
                     setActive(true);
                 }
+            }
+        }
+
+        if(shouldExplode()){
+            if(!world.isRemote){
+                boolean flag = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.world, this);
+                float f = 3.0F;
+                this.world.createExplosion(this, this.posX, this.posY, this.posZ, f, flag);
+                setShouldExplode(false);
+                setHasExploded(true);
             }
         }
 
@@ -70,8 +111,10 @@ public class EntitySentinel extends EntityGuardsman
 
         if (!world.isRemote) {
             // System.out.println(getAttackTarget());
-          //   System.out.println(isActive());
+            // System.out.println("Is active? " + isActive());
             // System.out.println(targetDistance);
+            //System.out.println("Has Exploded? " + hasExploded());
+          //  System.out.println("Should Explode? " + shouldExplode());
         }
         if (getAttackTarget() != null && isActive()) {
             getNavigator().tryMoveToEntityLiving(getAttackTarget(), .2D);
@@ -84,18 +127,83 @@ public class EntitySentinel extends EntityGuardsman
              motionY = 0;
         }
 
+        if(isActive()){
+            if (this.isEntityAlive()) {
+                boolean minHeight = world.getBlockState(getPosition().down(2)) == Blocks.AIR.getDefaultState();
+                boolean maxHeight = world.getBlockState(getPosition().down(4)) != Blocks.AIR.getDefaultState();
+                if (!minHeight)
+                    motionY += .1;
+                else if (!maxHeight)
+                    motionY -= .1;
+                else
+                    motionY = 0;
+
+                if (!world.isRemote) {
+                    if (getCharging() && chargeTicks < 40) {
+                        chargeTicks++;
+                        setChargeTicks(chargeTicks);
+                    }
+
+                    if (chargeTicks >= 40 && !getCharging()) {
+                        setChargeTicks(0);
+                        chargeTicks = 0;
+                    }
+                }
+
+                ticksSinceLastAttack++;
+                setTicksSinceLastAttack(ticksSinceLastAttack);
+
+                boolean thirtyPercent = rand.nextInt(3) == 0;
+
+                if (!world.isRemote && getAttackTarget() != null) {
+                    if (getTicksSinceLastAttack() == 20 && thirtyPercent) {
+                        resetStuff();
+                    } else if (getTicksSinceLastAttack() == 40 && thirtyPercent) {
+                        resetStuff();
+                    } else if (getTicksSinceLastAttack() == 60 && thirtyPercent) {
+                        resetStuff();
+                    } else if (getTicksSinceLastAttack() >= 80) {
+                        resetStuff();
+                    }
+                }
+            }
+            if (!this.world.isRemote && this.getAttackTarget() != null) {
+                Entity entity = this.getAttackTarget();
+
+                this.getLookHelper().setLookPositionWithEntity(getAttackTarget(), 10.0f,
+                        (float) this.getVerticalFaceSpeed());
+
+                if (entity != null) {
+                    double d0 = entity.posX - this.posX;
+                    double d1 = entity.posZ - this.posZ;
+                    double d3 = d0 * d0 + d1 * d1;
+
+                    if (d3 > 9.0D) {
+                        double d5 = (double) MathHelper.sqrt(d3);
+                        this.motionX += (d0 / d5 * 0.25D - this.motionX) * 0.1000000238418579D;
+                        this.motionZ += (d1 / d5 * 0.25D - this.motionZ) * 0.1000000238418579D;
+                    }
+                }
+
+                this.rotationYaw = (float) MathHelper.atan2(this.motionZ, this.motionX) * (180F / (float) Math.PI) - 90.0F;
+            }
+        }
     }
 
     @Override
     public void writeEntityToNBT(NBTTagCompound compound) {
         super.writeEntityToNBT(compound);
         compound.setBoolean("Active", isActive());
+        compound.setBoolean("ShouldExplode", shouldExplode());
+        compound.setBoolean("HasExploded", hasExploded());
     }
 
     @Override
     public void readEntityFromNBT(NBTTagCompound compound) {
         super.readEntityFromNBT(compound);
         setActive(compound.getBoolean("Active"));
+        setShouldExplode(compound.getBoolean("ShouldExplode"));
+        setHasExploded(compound.getBoolean("HasExploded"));
     }
 
     @Override
--- src/main/java/xenoscape/hailstorm/entity/sentinel/ModelSentinel.java
+++ src/main/java/xenoscape/hailstorm/entity/sentinel/ModelSentinel.java
@@ -3,22 +3,134 @@ package xenoscape.hailstorm.entity.sentinel;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
-import xenoscape.hailstorm.entity.guardsman.ModelGuardsman;
+import xenoscape.hailstorm.util.AdvancedModelBase;
+import xenoscape.hailstorm.util.AdvancedModelRenderer;
 
 /**
  * Cloudboi - T3DK Created using Tabula 6.0.0
  */
-public class ModelSentinel extends ModelGuardsman {
+public class ModelSentinel extends AdvancedModelBase {
 
-    public float defaultRotationX, defaultRotationY, defaultRotationZ;
-    public float defaultOffsetX, defaultOffsetY, defaultOffsetZ;
-    public float defaultPositionX, defaultPositionY, defaultPositionZ;
+    public AdvancedModelRenderer Head;
+    public AdvancedModelRenderer Float1;
+    public AdvancedModelRenderer Shield1;
+    public AdvancedModelRenderer Float3;
+    public AdvancedModelRenderer Float2;
+    public AdvancedModelRenderer Shield2;
+    public AdvancedModelRenderer Shield3;
+    public AdvancedModelRenderer Shield4;
+    public AdvancedModelRenderer Pole1A;
+    public AdvancedModelRenderer Pole1B;
+    public AdvancedModelRenderer Pole2A;
+    public AdvancedModelRenderer Pole2B;
+    public AdvancedModelRenderer Pole3A;
+    public AdvancedModelRenderer Pole3B;
+    public AdvancedModelRenderer Pole4A;
+    public AdvancedModelRenderer Pole4B;
+
+    public ModelSentinel() {
+        this.textureWidth = 128;
+        this.textureHeight = 64;
+        this.Shield2 = new AdvancedModelRenderer(this, 0, 48);
+        this.Shield2.setRotationPoint(0.0F, 3.0F, 0.1F);
+        this.Shield2.addBox(-1.4F, -5.0F, 7.2F, 3, 14, 1, 0.0F);
+        this.setRotateAngle(Shield2, 0.0F, -2.367539130330308F, 0.0F);
+        this.Float1 = new AdvancedModelRenderer(this, 0, 0);
+        this.Float1.setRotationPoint(0.0F, 8.5F, 0.0F);
+        this.Float1.addBox(-3.0F, 0.0F, -3.0F, 6, 1, 6, 0.0F);
+        this.Shield4 = new AdvancedModelRenderer(this, 0, 48);
+        this.Shield4.setRotationPoint(0.0F, 3.0F, 0.1F);
+        this.Shield4.addBox(-1.4F, -5.0F, 7.2F, 3, 14, 1, 0.0F);
+        this.setRotateAngle(Shield4, 0.0F, 0.7740535232594852F, 0.0F);
+        this.Pole4A = new AdvancedModelRenderer(this, 8, 48);
+        this.Pole4A.setRotationPoint(1.1F, -5.0F, 7.6F);
+        this.Pole4A.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole4A, 0.0F, 0.7853981633974483F, 0.0F);
+        this.Head = new AdvancedModelRenderer(this, 0, 0);
+        this.Head.setRotationPoint(0.0F, 10.0F, 0.0F);
+        this.Head.addBox(-4.0F, 0.0F, -4.0F, 8, 8, 8, 0.0F);
+        this.Pole3B = new AdvancedModelRenderer(this, 0, 48);
+        this.Pole3B.setRotationPoint(-1.9F, -5.0F, 6.1F);
+        this.Pole3B.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole3B, 0.0F, -0.7853981633974483F, 0.0F);
+        this.Pole4B = new AdvancedModelRenderer(this, 0, 48);
+        this.Pole4B.setRotationPoint(-1.9F, -5.0F, 6.1F);
+        this.Pole4B.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole4B, 0.0F, -0.7853981633974483F, 0.0F);
+        this.Pole1A = new AdvancedModelRenderer(this, 8, 48);
+        this.Pole1A.setRotationPoint(1.1F, -5.0F, 7.6F);
+        this.Pole1A.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole1A, 0.0F, 0.7853981633974483F, 0.0F);
+        this.Shield1 = new AdvancedModelRenderer(this, 0, 48);
+        this.Shield1.setRotationPoint(0.0F, 3.0F, 0.1F);
+        this.Shield1.addBox(-1.4F, -5.0F, 7.2F, 3, 14, 1, 0.0F);
+        this.setRotateAngle(Shield1, 0.0F, 2.356194490192345F, 0.0F);
+        this.Float2 = new AdvancedModelRenderer(this, 0, 0);
+        this.Float2.setRotationPoint(0.0F, 10.0F, 0.0F);
+        this.Float2.addBox(-2.0F, 0.0F, -2.0F, 4, 1, 4, 0.0F);
+        this.Float3 = new AdvancedModelRenderer(this, 0, 0);
+        this.Float3.setRotationPoint(0.0F, 11.5F, 0.0F);
+        this.Float3.addBox(-1.0F, 0.0F, -1.0F, 2, 1, 2, 0.0F);
+        this.Pole2B = new AdvancedModelRenderer(this, 0, 48);
+        this.Pole2B.setRotationPoint(-1.9F, -5.0F, 6.1F);
+        this.Pole2B.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole2B, 0.0F, -0.7853981633974483F, 0.0F);
+        this.Pole1B = new AdvancedModelRenderer(this, 0, 48);
+        this.Pole1B.setRotationPoint(-1.9F, -5.0F, 6.1F);
+        this.Pole1B.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole1B, 0.0F, -0.7853981633974483F, 0.0F);
+        this.Pole3A = new AdvancedModelRenderer(this, 8, 48);
+        this.Pole3A.setRotationPoint(1.1F, -5.0F, 7.6F);
+        this.Pole3A.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole3A, 0.0F, 0.7853981633974483F, 0.0F);
+        this.Shield3 = new AdvancedModelRenderer(this, 0, 48);
+        this.Shield3.setRotationPoint(0.0F, 3.0F, 0.1F);
+        this.Shield3.addBox(-1.4F, -5.0F, 7.2F, 3, 14, 1, 0.0F);
+        this.setRotateAngle(Shield3, 0.0F, -0.8196066167365371F, 0.0F);
+        this.Pole2A = new AdvancedModelRenderer(this, 8, 48);
+        this.Pole2A.setRotationPoint(1.1F, -5.0F, 7.6F);
+        this.Pole2A.addBox(0.0F, 0.0F, 0.0F, 2, 14, 2, 0.0F);
+        this.setRotateAngle(Pole2A, 0.0F, 0.7853981633974483F, 0.0F);
+        this.Head.addChild(this.Shield2);
+        this.Head.addChild(this.Float1);
+        this.Head.addChild(this.Shield4);
+        this.Shield4.addChild(this.Pole4A);
+        this.Shield3.addChild(this.Pole3B);
+        this.Shield4.addChild(this.Pole4B);
+        this.Shield1.addChild(this.Pole1A);
+        this.Head.addChild(this.Shield1);
+        this.Head.addChild(this.Float2);
+        this.Head.addChild(this.Float3);
+        this.Shield2.addChild(this.Pole2B);
+        this.Shield1.addChild(this.Pole1B);
+        this.Shield3.addChild(this.Pole3A);
+        this.Head.addChild(this.Shield3);
+        this.Shield2.addChild(this.Pole2A);
+
+     //   updateDefaultPose();
+    }
+
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+        this.Head.render(f5);
+    }
+
+    /**
+     * This is a helper function from Tabula to set the rotation of model parts
+     */
+    public void setRotateAngle(ModelRenderer AdvancedModelRenderer, float x, float y, float z) {
+        AdvancedModelRenderer.rotateAngleX = x;
+        AdvancedModelRenderer.rotateAngleY = y;
+        AdvancedModelRenderer.rotateAngleZ = z;
+    }
     
-    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
-                                  float headPitch, float scaleFactor, Entity entityIn) {
+    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn) {
         if (entityIn instanceof EntitySentinel) {
             EntitySentinel sentinel = (EntitySentinel) entityIn;
-            
+
+            if(sentinel.shouldExplode())
+                resetToDefaultPose();
+
             if (sentinel.isActive()) {
                 this.Head.rotateAngleX = headPitch / 50f;
                 this.Head.rotateAngleY = netHeadYaw / 50f;
@@ -50,8 +162,9 @@ public class ModelSentinel extends ModelGuardsman {
                     this.Head.rotationPointY = MathHelper.cos(0.1F * ageInTicks) * 5.0F + 12.5F;
                 }
             }
-            else{
-                this.Head.rotationPointY = 24;
+            else if(!sentinel.hasExploded() && !sentinel.isActive()){
+                this.Head.rotationPointY = 15;
+                this.Head.rotateAngleX = 10;
 
                 this.Shield1.rotationPointY = -1;
                 this.Shield1.rotationPointZ = 5;
@@ -78,64 +191,8 @@ public class ModelSentinel extends ModelGuardsman {
                 this.Float2.rotationPointY = -2;
                 this.Float3.rotationPointY = -3;
             }
+
         }
     }
 
-    /**
-     * Sets the current pose to the previously set default pose
-     */
-    public void resetToDefaultPose() {
-        this.boxList.stream().filter(modelRenderer -> modelRenderer instanceof ModelRenderer).forEach(modelRenderer -> {
-            ModelRenderer advancedModelRenderer = (ModelRenderer) modelRenderer;
-            this.resetToDefaultPose(advancedModelRenderer);
-        });
-    }
-
-    /**
-     * Sets this ModelRenderer's default pose to the current pose.
-     */
-    public void updateDefaultPose(ModelRenderer box) {
-        this.defaultRotationX = box.rotateAngleX;
-        this.defaultRotationY = box.rotateAngleY;
-        this.defaultRotationZ = box.rotateAngleZ;
-
-        this.defaultOffsetX = box.offsetX;
-        this.defaultOffsetY = box.offsetY;
-        this.defaultOffsetZ = box.offsetZ;
-
-        this.defaultPositionX = box.rotationPointX;
-        this.defaultPositionY = box.rotationPointY;
-        this.defaultPositionZ = box.rotationPointZ;
-    }
-    
-    /**
-     * Sets the current pose to the previously set default pose.
-     */
-    public void resetToDefaultPose(ModelRenderer box) {
-        box.rotateAngleX = this.defaultRotationX;
-        box.rotateAngleY = this.defaultRotationY;
-        box.rotateAngleZ = this.defaultRotationZ;
-
-        box.offsetX = this.defaultOffsetX;
-        box.offsetY = this.defaultOffsetY;
-        box.offsetZ = this.defaultOffsetZ;
-
-        box.rotationPointX = this.defaultPositionX;
-        box.rotationPointY = this.defaultPositionY;
-        box.rotationPointZ = this.defaultPositionZ;
-    }
-
-    public void transitionTo(ModelRenderer to, float timer, float maxTime) {
-        to.rotateAngleX += ((to.rotateAngleX - to.rotateAngleX) / maxTime) * timer;
-        to.rotateAngleY += ((to.rotateAngleY - to.rotateAngleY) / maxTime) * timer;
-        to.rotateAngleZ += ((to.rotateAngleZ - to.rotateAngleZ) / maxTime) * timer;
-
-        to.rotationPointX += ((to.rotationPointX - to.rotationPointX) / maxTime) * timer;
-        to.rotationPointY += ((to.rotationPointY - to.rotationPointY) / maxTime) * timer;
-        to.rotationPointZ += ((to.rotationPointZ - to.rotationPointZ) / maxTime) * timer;
-
-        to.offsetX += ((to.offsetX - to.offsetX) / maxTime) * timer;
-        to.offsetY += ((to.offsetY - to.offsetY) / maxTime) * timer;
-        to.offsetZ += ((to.offsetZ - to.offsetZ) / maxTime) * timer;
-    }
 }
