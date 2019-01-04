1bcfe2270d276105bab51b85eb7e72b9fe82cf1d
Gabriel <gnangel26@gmail.com>
2019-01-03 19:43:35 -0500

    mooar!

--- src/main/java/xenoscape/worldsretold/heatwave/entity/neutral/scorpion/EntityScorpion.java
+++ src/main/java/xenoscape/worldsretold/heatwave/entity/neutral/scorpion/EntityScorpion.java
@@ -61,12 +61,10 @@ public class EntityScorpion extends EntitySurfaceMonster
     {
         this.tasks.addTask(1, new EntityAISwimming(this));
         this.tasks.addTask(1, new EntityAIRestrictSun(this));
-        this.tasks.addTask(2, new EntityAIFleeSun(this, 1.0D));
-        this.tasks.addTask(3, new EntityAILeapAtTarget(this, 0.4F));
+        this.tasks.addTask(2, new EntityAISeekShelter(this, 1.2D));
+        this.tasks.addTask(3, new EntityAILeapAtTarget(this, 0.5F));
         this.tasks.addTask(4, new EntityScorpion.AIScorpionAttack(this));
         this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 0.8D));
-        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(6, new EntityAILookIdle(this));
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false, new Class[0]));
         this.targetTasks.addTask(2, new EntityScorpion.AIScorpionTarget(this, EntityPlayer.class));
         this.targetTasks.addTask(3, new EntityScorpion.AIScorpionTarget(this, EntityIronGolem.class));
