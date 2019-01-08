8008a0950fc69b49716222edf2c079d7317371d8
Gabriel <gnangel26@gmail.com>
2019-01-08 10:23:15 -0500

    Update ModelMummy.java

--- src/main/java/xenoscape/worldsretold/heatwave/entity/hostile/mummy/ModelMummy.java
+++ src/main/java/xenoscape/worldsretold/heatwave/entity/hostile/mummy/ModelMummy.java
@@ -11,7 +11,7 @@ import net.minecraft.util.EnumHandSide;
 import net.minecraft.util.math.MathHelper;
 
 /**
- * Mummy - Undefined
+ * Mummy - MCvinnyq
  * Created using Tabula 7.0.0
  */
 public class ModelMummy extends ModelBase {
@@ -21,19 +21,16 @@ public class ModelMummy extends ModelBase {
     public ModelRenderer LeftArm;
     public ModelRenderer RightLeg;
     public ModelRenderer LeftLeg;
-    public ModelRenderer Cloth1;
     public ModelRenderer Hat;
     public ModelRenderer Head;
     public ModelRenderer Cloth4;
     public ModelRenderer Cloth2;
     public ModelRenderer Cloth3;
+    public ModelRenderer Cloth1;
 
     public ModelMummy() {
         this.textureWidth = 64;
         this.textureHeight = 64;
-        this.Body1 = new ModelRenderer(this, 14, 16);
-        this.Body1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.Body1.addBox(-4.0F, 0.0F, -2.0F, 8, 6, 4, 0.0F);
         this.RightArm = new ModelRenderer(this, 38, 16);
         this.RightArm.setRotationPoint(-4.5F, 2.5F, 0.0F);
         this.RightArm.addBox(-2.0F, -2.0F, -1.5F, 3, 12, 3, 0.0F);
@@ -41,40 +38,44 @@ public class ModelMummy extends ModelBase {
         this.Cloth3 = new ModelRenderer(this, 50, 31);
         this.Cloth3.setRotationPoint(0.5F, 4.0F, 1.5F);
         this.Cloth3.addBox(-1.0F, -6.0F, 0.0F, 2, 12, 3, 0.0F);
+        this.Head = new ModelRenderer(this, 0, 0);
+        this.Head.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.Head.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, 0.0F);
+        this.Body1 = new ModelRenderer(this, 14, 16);
+        this.Body1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.Body1.addBox(-4.0F, 0.0F, -2.0F, 8, 6, 4, 0.0F);
+        this.Cloth2 = new ModelRenderer(this, 50, 16);
+        this.Cloth2.setRotationPoint(-0.5F, 4.0F, 1.5F);
+        this.Cloth2.addBox(-1.0F, -6.0F, 0.0F, 2, 12, 3, 0.0F);
         this.Body2 = new ModelRenderer(this, 16, 26);
         this.Body2.setRotationPoint(0.0F, 6.0F, 0.0F);
         this.Body2.addBox(-3.0F, 0.0F, -2.0F, 6, 6, 4, 0.0F);
-        this.Hat = new ModelRenderer(this, 32, 0);
-        this.Hat.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.Hat.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, 0.25F);
-        this.Cloth4 = new ModelRenderer(this, 14, 36);
-        this.Cloth4.setRotationPoint(0.0F, 6.0F, 0.0F);
-        this.Cloth4.addBox(-4.0F, 0.0F, -2.0F, 8, 2, 4, 0.0F);
-        this.Cloth1 = new ModelRenderer(this, 24, 0);
-        this.Cloth1.setRotationPoint(0.0F, -6.0F, 4.25F);
-        this.Cloth1.addBox(-4.0F, -2.0F, 0.0F, 8, 8, 0, 0.0F);
-        this.setRotateAngle(Cloth1, 0.17453292519943295F, 0.0F, 0.0F);
         this.LeftLeg = new ModelRenderer(this, 0, 16);
         this.LeftLeg.mirror = true;
         this.LeftLeg.setRotationPoint(1.75F, 12.0F, 0.0F);
         this.LeftLeg.addBox(-1.5F, 0.0F, -2.0F, 3, 12, 4, 0.0F);
-        this.RightLeg = new ModelRenderer(this, 0, 16);
-        this.RightLeg.setRotationPoint(-1.75F, 12.0F, 0.0F);
-        this.RightLeg.addBox(-1.5F, 0.0F, -2.0F, 3, 12, 4, 0.0F);
+        this.Cloth1 = new ModelRenderer(this, 24, 0);
+        this.Cloth1.setRotationPoint(0.0F, -6.0F, 4.25F);
+        this.Cloth1.addBox(-4.0F, -2.0F, 0.0F, 8, 8, 0, 0.0F);
+        this.setRotateAngle(Cloth1, 0.17453292519943295F, 0.0F, 0.0F);
         this.LeftArm = new ModelRenderer(this, 38, 16);
         this.LeftArm.mirror = true;
         this.LeftArm.setRotationPoint(4.5F, 2.5F, 0.0F);
         this.LeftArm.addBox(-1.0F, -2.0F, -1.5F, 3, 12, 3, 0.0F);
         this.setRotateAngle(LeftArm, -1.3089969389957472F, -0.08726646259971647F, 0.0F);
-        this.Cloth2 = new ModelRenderer(this, 50, 16);
-        this.Cloth2.setRotationPoint(-0.5F, 4.0F, 1.5F);
-        this.Cloth2.addBox(-1.0F, -6.0F, 0.0F, 2, 12, 3, 0.0F);
-        this.Head = new ModelRenderer(this, 0, 0);
-        this.Head.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.Head.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, 0.0F);
+        this.Hat = new ModelRenderer(this, 32, 0);
+        this.Hat.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.Hat.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, 0.25F);
+        this.RightLeg = new ModelRenderer(this, 0, 16);
+        this.RightLeg.setRotationPoint(-1.75F, 12.0F, 0.0F);
+        this.RightLeg.addBox(-1.5F, 0.0F, -2.0F, 3, 12, 4, 0.0F);
+        this.Cloth4 = new ModelRenderer(this, 14, 36);
+        this.Cloth4.setRotationPoint(0.0F, 6.0F, 0.0F);
+        this.Cloth4.addBox(-4.0F, 0.0F, -2.0F, 8, 2, 4, 0.0F);
         this.LeftArm.addChild(this.Cloth3);
-        this.Body1.addChild(this.Cloth4);
         this.RightArm.addChild(this.Cloth2);
+        this.Hat.addChild(this.Cloth1);
+        this.Body1.addChild(this.Cloth4);
     }
 
     @Override
@@ -191,6 +192,9 @@ public class ModelMummy extends ModelBase {
         this.LeftArm.rotateAngleX -= MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
 
         copyModelAngles(this.Head, this.Hat);
+        copyModelAngles(this.Head, this.Cloth1);
+        this.Cloth1.rotateAngleX += 0.17453292519943295F;
+        
         boolean flags = entityIn instanceof EntityMummy && ((EntityMummy)entityIn).isArmsRaised();
         float fn = MathHelper.sin(this.swingProgress * (float)Math.PI);
         float f1 = MathHelper.sin((1.0F - (1.0F - this.swingProgress) * (1.0F - this.swingProgress)) * (float)Math.PI);
