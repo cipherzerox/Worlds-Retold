125fe1b19aa349aaa152953006cd18d986bef3f9
Gabriel <gnangel26@gmail.com>
2019-01-10 15:48:01 -0500

    Revert "let's make the wither effect brewable!"
    This reverts commit e63e1f7d7426046686872f68f0a56f2feff753ae.

--- src/main/java/xenoscape/worldsretold/heatwave/init/HeatwavePotions.java
+++ src/main/java/xenoscape/worldsretold/heatwave/init/HeatwavePotions.java
@@ -2,9 +2,7 @@ package xenoscape.worldsretold.heatwave.init;
 
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.init.Items;
-import net.minecraft.init.MobEffects;
 import net.minecraft.init.PotionTypes;
-import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.potion.PotionHelper;
@@ -24,17 +22,9 @@ public class HeatwavePotions {
 			new PotionEffect[] { new PotionEffect(VENOM, 1800) }).setRegistryName("long_venom");
 	public static final PotionType STRONG_VENOM = new PotionType("venom",
 			new PotionEffect[] { new PotionEffect(VENOM, 450, 1) }).setRegistryName("strong_venom");
-	
-	public static final PotionType NORMAL_WITHER = new PotionType("wither",
-			new PotionEffect[] { new PotionEffect(MobEffects.WITHER, 1200) }).setRegistryName("wither");
-	public static final PotionType LONG_WITHER = new PotionType("wither",
-			new PotionEffect[] { new PotionEffect(MobEffects.WITHER, 2400) }).setRegistryName("wither_venom");
-	public static final PotionType STRONG_WITHER = new PotionType("wither",
-			new PotionEffect[] { new PotionEffect(MobEffects.WITHER, 600, 1) }).setRegistryName("wither_venom");
 
 	public static void registerPotions() {
 		registerPotion(NORMAL_VENOM, LONG_VENOM, STRONG_VENOM, VENOM);
-		registerPotion(NORMAL_WITHER, LONG_WITHER, STRONG_WITHER, MobEffects.WITHER);
 		registerPotionRecipes();
 	}
 
@@ -51,8 +41,5 @@ public class HeatwavePotions {
 		PotionHelper.addMix(NORMAL_VENOM, Items.REDSTONE, LONG_VENOM);
 		PotionHelper.addMix(NORMAL_VENOM, Items.GLOWSTONE_DUST, STRONG_VENOM);
 		PotionHelper.addMix(NORMAL_VENOM, Items.FERMENTED_SPIDER_EYE, PotionTypes.REGENERATION);
-		PotionHelper.addMix(PotionTypes.AWKWARD, new ItemStack(Items.SKULL, 1, 1).getItem(), NORMAL_WITHER);
-		PotionHelper.addMix(NORMAL_WITHER, Items.REDSTONE, LONG_WITHER);
-		PotionHelper.addMix(NORMAL_WITHER, Items.GLOWSTONE_DUST, STRONG_WITHER);
 	}
 }
