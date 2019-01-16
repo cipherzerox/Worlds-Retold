0b5d6761dbf3b7fbc9094bf2055ea3d8e4941e24
Gabriel <gnangel26@gmail.com>
2019-01-15 19:04:55 -0500

    meow

--- src/main/java/xenoscape/worldsretold/defaultmod/basic/BasicItem.java
+++ src/main/java/xenoscape/worldsretold/defaultmod/basic/BasicItem.java
@@ -1,7 +1,9 @@
 package xenoscape.worldsretold.defaultmod.basic;
 
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -36,6 +38,8 @@ public class BasicItem extends Item implements ModelRegistry {
         {
         	if (!worldIn.isRemote && playerIn.getHealth() < playerIn.getMaxHealth())
         	{
+                EntityPlayerMP entityplayermp = (EntityPlayerMP)playerIn;
+                CriteriaTriggers.CONSUME_ITEM.trigger(entityplayermp, itemstack);
         		worldIn.playSound(null, playerIn.getPosition(), SoundEvents.ENTITY_LEASHKNOT_PLACE, playerIn.getSoundCategory(), 1F, 1.5F);
         		playerIn.heal(2F);
         		itemstack.shrink(1);
