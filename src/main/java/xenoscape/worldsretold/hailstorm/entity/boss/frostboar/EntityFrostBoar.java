ee3c55e4e7f2ad857f1e2c4a288f2cc545fdebcd
Gabriel <gnangel26@gmail.com>
2019-01-17 11:29:37 -0500

    more staff

--- src/main/java/xenoscape/worldsretold/hailstorm/entity/boss/frostboar/EntityFrostBoar.java
+++ src/main/java/xenoscape/worldsretold/hailstorm/entity/boss/frostboar/EntityFrostBoar.java
@@ -1,5 +1,12 @@
 package xenoscape.worldsretold.hailstorm.entity.boss.frostboar;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.BossInfo;
+import net.minecraft.world.BossInfoServer;
 import net.minecraft.world.World;
 import xenoscape.worldsretold.defaultmod.basic.EntitySurfaceMonster;
 import xenoscape.worldsretold.defaultmod.util.EntityElementalLookHelper;
@@ -7,9 +14,125 @@ import xenoscape.worldsretold.hailstorm.entity.ISnowCreature;
 
 public class EntityFrostBoar extends EntitySurfaceMonster implements ISnowCreature 
 {
+    private final BossInfoServer bossInfo = (BossInfoServer)(new BossInfoServer(this.getDisplayName(), BossInfo.Color.WHITE, BossInfo.Overlay.PROGRESS));
+    
 	public EntityFrostBoar(World world) 
 	{
 		super(world);
 		setSize(1F, 6F);
+		this.experienceValue = 50;
 	}
+
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(150.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(8.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(40.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(8.0D);
+    }
+	
+    /**
+     * (abstract) Protected helper method to write subclass entity data to NBT.
+     */
+    public void writeEntityToNBT(NBTTagCompound compound)
+    {
+        super.writeEntityToNBT(compound);
+    }
+
+    /**
+     * (abstract) Protected helper method to read subclass entity data from NBT.
+     */
+    public void readEntityFromNBT(NBTTagCompound compound)
+    {
+        super.readEntityFromNBT(compound);
+
+        if (this.hasCustomName())
+        {
+            this.bossInfo.setName(this.getDisplayName());
+        }
+    }
+    
+    /**
+     * Sets the custom name tag for this entity
+     */
+    public void setCustomNameTag(String name)
+    {
+        super.setCustomNameTag(name);
+        this.bossInfo.setName(this.getDisplayName());
+    }
+    
+    /**
+     * Sets the Entity inside a web block.
+     */
+    public void setInWeb()
+    {
+    }
+
+    /**
+     * Add the given player to the list of players tracking this entity. For instance, a player may track a boss in
+     * order to view its associated boss bar.
+     */
+    public void addTrackingPlayer(EntityPlayerMP player)
+    {
+        super.addTrackingPlayer(player);
+        this.bossInfo.addPlayer(player);
+    }
+
+    /**
+     * Removes the given player from the list of players tracking this entity. See {@link Entity#addTrackingPlayer} for
+     * more information on tracking.
+     */
+    public void removeTrackingPlayer(EntityPlayerMP player)
+    {
+        super.removeTrackingPlayer(player);
+        this.bossInfo.removePlayer(player);
+    }
+
+    public void fall(float distance, float damageMultiplier)
+    {
+    }
+
+    /**
+     * adds a PotionEffect to the entity
+     */
+    public void addPotionEffect(PotionEffect potioneffectIn)
+    {
+    }
+
+    /**
+     * Makes the entity despawn if requirements are reached
+     */
+    protected void despawnEntity()
+    {
+        this.idleTime = 0;
+    }
+
+    protected boolean canBeRidden(Entity entityIn)
+    {
+        return false;
+    }
+
+    /**
+     * Returns false if this Entity is a boss, true otherwise.
+     */
+    public boolean isNonBoss()
+    {
+        return false;
+    }
+    
+    protected void updateAITasks()
+    {
+    	super.updateAITasks();
+    }
+    
+    /**
+     * Called frequently so the entity can update its state every tick as required. For example, zombies and skeletons
+     * use this to react to sunlight and start to burn.
+     */
+    public void onLivingUpdate()
+    {
+    	super.onLivingUpdate();
+    }
 }
