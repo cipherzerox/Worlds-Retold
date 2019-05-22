873bead285c5dfb370e06cebf4680651a8b128f0
cipherzerox <cipherzerox@gmail.com>
2019-05-22 13:36:41 -0500

    Create HellfireBlocks.java

---
+++ src/main/java/xenoscape/worldsretold/hellfire/init/HellfireBlocks.java
@@ -0,0 +1,8 @@
+package xenoscape.worldsretold.hellfire.init;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import xenoscape.worldsretold.WorldsRetold;
+
+@GameRegistry.ObjectHolder(WorldsRetold.MODID)
+public class HellfireBlocks {
+}
