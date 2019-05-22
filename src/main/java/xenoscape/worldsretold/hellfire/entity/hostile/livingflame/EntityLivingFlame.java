76a57a30ccaf1cb102e9e2a2ac207ff8d41b1a5b
Gabriel <gnangel26@gmail.com>
2019-05-22 10:44:20 -0400

    Update EntityLivingFlame.java

--- src/main/java/xenoscape/worldsretold/hellfire/entity/hostile/livingflame/EntityLivingFlame.java
+++ src/main/java/xenoscape/worldsretold/hellfire/entity/hostile/livingflame/EntityLivingFlame.java
@@ -45,6 +45,7 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -100,7 +101,6 @@ public class EntityLivingFlame extends EntitySurfaceMonster implements INetherCr
     @Nullable
     public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata)
     {
-        this.posY += 0.9D;
 		this.prevRenderYawOffset = this.renderYawOffset = this.prevRotationYaw = this.rotationYaw = this.prevRotationYawHead = this.rotationYawHead = 180F;
         return super.onInitialSpawn(difficulty, livingdata);
     }
@@ -315,9 +315,27 @@ public class EntityLivingFlame extends EntitySurfaceMonster implements INetherCr
             this.extraheight = MathHelper.clamp(this.extraheight - 0.1F, 0F, height2 ? 0.1F : height ? 0.4F : height4 ? 0.9F : height3 ? 1.4F : 1.9F);
         }
 
-        this.setSize(0.1F, 0.1F);
+        this.setSize(0.75F, 0.1F);
         if (this.world.isRemote)
             this.setSize(0.75F, 0.1F + this.extraheight);
+        
+        if (this.isEntityAlive() && (this.isAggressive() || !this.onGround))
+        {
+            double d0 = (double)this.getPosition().getX() + rand.nextDouble();
+            double d1 = (double)this.getPosition().getY() - 1D + rand.nextDouble() * 0.5D + this.extraheight;
+            double d2 = (double)this.getPosition().getZ() + rand.nextDouble();
+            world.spawnParticle(EnumParticleTypes.SMOKE_LARGE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+        }
+        else if (!this.isEntityAlive())
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                double d0 = (double)this.getPosition().getX() + rand.nextDouble();
+                double d1 = (double)this.getPosition().getY() + rand.nextDouble() * 0.5D + 0.5D;
+                double d2 = (double)this.getPosition().getZ() + rand.nextDouble();
+                world.spawnParticle(EnumParticleTypes.SMOKE_LARGE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            }
+        }
     }
     
 	public boolean getCanSpawnHere() 
